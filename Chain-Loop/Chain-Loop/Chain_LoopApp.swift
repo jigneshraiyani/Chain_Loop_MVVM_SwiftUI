866efdd4c30a96fc6fb5547e35995c3a2a693129
Jignesh Raiyani <jignesh.raiyani1@gmail.com>
2024-12-28 08:41:37 +0530

    ID_1234 project template

---
+++ Chain-Loop/Chain-Loop/Chain_LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chain_LoopApp.swift
+//  Chain-Loop
+//
+//  Created by Raiyani Jignesh on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Chain_LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
